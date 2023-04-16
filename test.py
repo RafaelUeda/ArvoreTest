b12212fd22e3dd9e8bb4ac1eca6f9669d9e4ed9c
Rafael Felipe Ueda <124635574+RafaelUeda@users.noreply.github.com>
2023-04-16 14:57:50 -0300

    Att prova

--- test.py
+++ test.py
@@ -884,3 +884,140 @@ Busca pela chave 10: Sucesso!
 Busca pela chave 30: Sucesso!
 Busca pela chave 70: Sucesso!
 Busca pela chave 100: Falha!
+
+# Mais códigos #
+
+class BSTNode(object):
+    def __init__(self, key, value=None, left=None, right=None):
+        self.key = key
+        self.value = value
+        self.left = left
+        self.right = right
+        
+# Construção de um nó, com chave 42
+root = BSTNode(42) 
+# 42
+
+# Adicionando filhos ao nó 42
+root.left = BSTNode(10)
+root.right = BSTNode(90)
+#    42
+#   /  \
+# 10    90
+
+# Adicionando um filho esquerdo ao nó de valor 10
+root.left.left = BSTNode(2)
+#       42
+#      /  \
+#    10    90
+#   / 
+#  2  
+    
+    def get(self, key):
+        if key < self.key:
+            return self.left.get(key) if self.left else None
+        elif key > self.key:
+            return self.right.get(key) if self.right else None
+        else:
+            return self
+        
+# Para realizar uma busca pela chave 4
+
+tree = BSTNode(8)
+...
+found = tree.get(4)
+if found:
+    print(found)
+
+# O método get apresentado acima poderia ser refatorado, evitando a double código #
+
+def get(self, key):
+    """Retorna uma referência ao nó de chave key"""
+    if self.key == key:
+        return self
+    node = self.left if key < self.key else self.right
+    if node is not None:
+        return node.get(key)
+    
+# Método de inserção #
+
+    def add(self, node):
+        if node.value < self.value:
+            if self.left is None:
+                self.left = node
+            else:
+                self.left.add(node)
+        else:
+            if self.right is None:
+                self.right = node
+            else:
+                self.right.add(node)
+                
+# Refatorando para diminuir #
+
+    def add(self, key):
+    """Adiciona elemento à subárvore"""
+    side = 'left' if key < self.key else 'right'
+    node = getattr(self, side)
+    if node is None:
+        setattr(self, side, BSTNode(key))
+    else:
+        node.add(key) 
+       
+tree = BSTNode(0)
+for i in range(1, 10):
+    tree.add(i)
+    
+# Remover, mínimo, remover mínimo #
+
+    def remove(self, key):
+        if key < self.key:
+            self.left = self.left.remove(key)
+        elif key > self.key:
+            self.right = self.right.remove(key)
+        else:
+            # encontramos o elemento, então vamos removê-lo!
+            if self.right is None:
+                return self.left
+            if self.left is None:
+                return self.right
+            #ao invés de remover o nó, copiamos os valores do nó substituto
+            tmp = self.right._min()
+            self.key, self.value = tmp.key, tmp.value
+            self.right._remove_min()
+        return self
+ 
+    def _min(self):
+        """Retorna o menor elemento da subárvore que tem self como raiz.
+        """
+        if self.left is None:
+            return self
+        else:
+            return self.left._min()
+ 
+    def _remove_min(self):
+        """Remove o menor elemento da subárvore que tem self como raiz.
+        """
+        if self.left is None:  # encontrou o min, daí pode rearranjar
+            return self.right
+        self.left = self.left._removeMin()
+        return self
+    
+# Método que implementa as 3 possíveis estratégias para visitar todos os nós da árvore #
+
+    def traverse(self, visit, order='pre'):
+    """Percorre a árvore na ordem fornecida como parâmetro (pre, pos ou in)
+       visitando os nós com a função visit() recebida como parâmetro.
+    """
+    if order == 'pre':
+        visit(self)
+    if self.left is not None:
+        self.left.traverse(visit, order)
+    if order == 'in':
+        visit(self)
+    if self.right is not None:
+        self.right.traverse(visit, order)
+    if order == 'post':
+        visit(self)
+
+tree.traverse(print, 'in')
