ad2c84cfef643d9355f9286f8f295baa6d613d94
Rafael Felipe Ueda <124635574+RafaelUeda@users.noreply.github.com>
2023-04-16 14:32:27 -0300

    Códigos Testes

--- test.py
+++ test.py
@@ -750,3 +750,137 @@ def dijkstra(G, start):
 
 D = dijkstra(G, 'a')
 print(D)
+
+# Códigos adicionais para a prova #
+
+
+class NodoArvore: # Representação de Árvores Binárias #
+    def __init__(self, chave=None, esquerda=None, direita=None):
+        self.chave = chave
+        self.esquerda = esquerda
+        self.direita = direita
+        
+    def __repr__(self):
+        return '%s <- %s -> %s' % (self.esquerda and self.esquerda.chave, 
+                                   self.chave,
+                                   self.direita and self.direita.chave)
+    
+raiz = NodoArvore(3) # Criando nodos de uma árvore utilizando o código acima #
+raiz.esquerda = NodoArvore(5)
+raiz.direita = NodoArvore(1)
+print("Árvore: ", raiz)
+    
+# Caminhamento em ordem, primeiro construiremos uma árvore binária de pesquisa #
+
+raiz = NodoArvore(40)
+
+raiz.esquerda = NodoArvore(20)
+raiz.direita = NodoArvore(60)
+
+raiz.direita.esquerda = NodoArvore(50)
+raiz.direita.direita = NodoArvore(70)
+raiz.esquerda.esquerda = NodoArvore(10)
+raiz.esquerda.direita = NodoArvore(30)
+
+# Como implementar o caminhamento em ordem #
+
+def em_ordem(raiz):
+    if not raiz:
+        return
+    
+    # Visita filho na esquerda.
+    em_ordem(raiz.esquerda)
+    
+    # Visita nodo corrente.
+    print(raiz.chave),
+    
+    # Visita filho da direita.
+    em_ordem(raiz.direita)
+    
+# Executando o código acima na árvore criada anteriormente #
+
+em_ordem(raiz)
+
+10
+20
+30
+40
+50
+60
+70
+
+# Inserção em Árvores Binárias de Pesquisa #
+
+def insere(raiz, nodo):
+    """Insere um nodo em uma árvore binária de pesquisa."""
+    # Nodo deve ser inserido na raiz.
+    if raiz is None:
+        raiz = nodo
+        
+    # Nodo deve ser inserido na subárvore direita.
+    elif raiz.chave < nodo.chave:
+        if raiz.direita is None:
+            raiz.direita = nodo
+        else:
+            insere(raiz.direita, nodo)
+            
+    # Nodo deve ser inserido na subárvore esquerda.
+    else:
+        if raiz.esquerda is None:
+            raiz.esquerda = nodo
+        else: 
+            insere(raiz.esquerda, nodo)
+            
+# Função de inserção que acabamos de desenvolver #
+
+# Cria uma árvore binária de pesquisa.
+raiz = NodoArvore(40)
+for chave in [20, 60, 50, 70, 10, 30]:
+    nodo = NodoArvore(chave)
+    insere(raiz, nodo)
+# Imprime o caminhamento em ordem da árvore.
+em_ordem(raiz)
+
+10
+20
+30
+40
+50
+60
+70
+
+# Busca de Árvores Binárias em Pesquisa #
+    
+def busca(raiz, chave):
+    """Procura por uma chave em uma árvore binária de pesquisa."""
+    #Trata o caso em que a chave procurada não está presente.
+    if raiz is None:
+        return None
+    
+    # A chave procurada está na raiz da árvore.
+    if raiz.chave == chave:
+        return raiz
+    
+    # A chave procurada é maior que a da raiz.
+    if raiz.chave < chave:
+        return busca(raiz.direita, chave)
+    
+    # A chave procurada é menor que a da raiz.
+    return busca(raiz.esquerda, chave)
+
+# O código abaixo testa nossa implementação de busca em árvores binárias de pesquisa #
+
+# Cria uma árvore binária de pesquisa.
+raiz = NodoArvore(40)
+for chave in [-50, 10, 30, 70, 100]:
+    resultado = busca(raiz, chave)
+    if resultado:
+        print("Busca pela chave {}: Sucesso!".format(chave))
+    else:
+        print("Busca pela chave {}: Falha!".format(chave))
+        
+Busca pela chave -50: Falha!
+Busca pela chave 10: Sucesso!
+Busca pela chave 30: Sucesso!
+Busca pela chave 70: Sucesso!
+Busca pela chave 100: Falha!
